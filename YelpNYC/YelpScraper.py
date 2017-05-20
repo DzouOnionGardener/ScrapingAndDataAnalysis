5cf5ea9b3009c2838562ca2c8771850f2ae3bc3a
dzou <dawenzou1@gmail.com>
2017-05-20 16:49:02 -0400

    added DB side stuff, need to fix unicode in order to work correctly

--- YelpNYC/YelpScraper.py
+++ YelpNYC/YelpScraper.py
@@ -2,6 +2,8 @@ import scrapy
 import requests
 from bs4 import BeautifulSoup
 import csv
+import time
+import MySQLdb
 """
 we're going to parse data from the first 100 pages
 store that data into CSV files
@@ -36,6 +38,7 @@ class Yelp(object):
         self.baseURL = "https://www.yelp.com/search?find_loc=New+York,+NY,+US&start="
         self.pageIndex = 0
         self.endURL = "&cflt=restaurants"
+        self.res = []
         with open('results.csv', 'w') as csvfile:
             self.writer = csv.writer(csvfile)
             self.writer.writerow(["Restaurant", "Rating", "Price_Range", "Area", "Address"])
@@ -47,26 +50,81 @@ class Yelp(object):
     # <span class="neighborhood-str-list>
     # <address>
     def generateCSV(self):
-        while(self.pageIndex < 200):
+        page = 1
+        while(self.pageIndex < 30):
+            print "reading page %s" % page
             url = self.baseURL + str(self.pageIndex) + self.endURL
             req = requests.get(url)
-            soup = BeautifulSoup(req.content)
+            soup = BeautifulSoup(req.content, "lxml")
             ##
-            bn = [d.span.contents for d in soup.find_all("a", {"class":"biz-name"})]
+
+            #bn = [d.span.contents[0] for d in soup.find_all("a", {"class":"biz-name"})]
+
+            bn = [d.span.contents for d in soup.find_all("a", {"class": "biz-name"})]
             rating = [d['title'].strip('star rating') for d in soup.find_all("div", {"class":"i-stars"})]
             price = [d.contents[0] for d in soup.find_all("span",{"class":"price-range"})]
             area = [d.contents[0].strip() for d in soup.find_all("span", {"class":"neighborhood-str-list"})]
             addr = [d.contents[0].strip() for d in soup.find_all("address")]
-            res = [d for d in zip(bn, rating, price, area, addr)]
+            self.res = [d for d in zip(bn, rating, price, area, addr)]
             with open('results.csv', 'a') as d:
                 self.writer = csv.writer(d)
                 try:
-                    for e in res:
+                    for e in self.res:
                         self.writer.writerow(e)
                 except:
                     pass
             self.pageIndex += 10
+            page += 1
+            time.sleep(3)
+        print "done"
 
+    def ImportToDataBase(self):
+        userName = raw_input("mySQL userName: ")
+        password = raw_input("mySQL password: ")
+        userName = str(userName)
+        password = str(password)
+        ##
+        ## address, username, password, database_name
+        DB = MySQLdb.connect("localhost", userName, password, "Restaurants")
+        #table = restaurantTable
+        #columns: name(varchar(255)), rating(decimal), price(varchar(6)), area(varchar(255)), addr(varchar(255))
+
+        cursor = DB.cursor()
+        ##used to handle database side actions
+        #query = "INSERT INTO restaurantTable(name, rating, price, area, addr) VALUES('%s', '%s', '%s', '%s', '%s' )"
+
+        #for elements in restaurants list
+        try:
+            for e in self.res:
+                cursor.execute("INSERT INTO Restaurants.restaurantTable(name, rating, price, area, addr) VALUES(%s, %s, %s, %s, %s )", e)
+                print "executed!"
+        except:
+            pass
+        print "done! closing connection."
+        cursor.close()
+        DB.close()
+
+
+    def printDB(self):
+        userName = raw_input("mySQL userName: ")
+        password = raw_input("mySQL password: ")
+        userName = str(userName)
+        password = str(password)
+        ##
+        ## address, username, password, database_name
+        DB = MySQLdb.connect("localhost", userName, password, "Restaurants")
+        print "connected"
+        cursor = DB.cursor()
+        cursor.execute("select * from Restaurants.restaurantTable")
+        print "printing"
+        results = cursor.fetchall()
+        print results
+        cursor.close()
+        DB.close()
+
+        print results
 if __name__ == "__main__":
     y = Yelp()
     y.generateCSV()
+    #y.ImportToDataBase()
+    #y.printDB()
