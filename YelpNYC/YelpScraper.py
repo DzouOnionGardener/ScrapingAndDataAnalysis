f35954bbc20aa6627f51839fb652d105e8fd13cf
dzou <dawenzou1@gmail.com>
2017-05-20 00:33:55 -0400

    exports to csv, restaurant name needs fixing though

--- YelpNYC/YelpScraper.py
+++ YelpNYC/YelpScraper.py
@@ -3,9 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-url = "https://www.yelp.com/search?cflt=restaurants&find_loc=New+York%2C+NY%2C+US"
+url = "https://www.yelp.com/search?find_loc=New+York,+NY,+US&start=0&cflt=restaurants"
+## 'start=' iterates by 10
 """
-we're going to use parse data from the first 20 pages
+we're going to parse data from the first 100 pages
 store that data into CSV files
 then we'll also store that data into a mySQL database
 execute some basic queries on it
@@ -22,7 +23,7 @@ soup = BeautifulSoup(req.content)
 #<div class="secondary-attribute">
 #<span class="neighborhood-str-list>
 #<address>
-
+"""
 bizName = soup.find_all("a", {"class":"biz-name"})
 bizRating = soup.find_all("div", {"class":"i-stars"})
 priceRange = soup.find_all("span",{"class":"price-range"})
@@ -32,15 +33,28 @@ bizAddr = soup.find_all("address")
 
 for names in bizName:
     print names.text
-
-for ratings in bizRating:
-    print ratings["title"]
-
+for r in bizRating:
+    print r["title"]
 for price in priceRange:
     print price.text
-
 for addr in bizAddr:
     print addr.text.lstrip()
-
 for n in bizNeighborhood:
     print n.text.lstrip()
+"""
+bn = [d.span.contents for d in soup.find_all("a", {"class":"biz-name"})]
+rating = [d['title'].strip('star rating') for d in soup.find_all("div", {"class":"i-stars"})]
+price = [d.contents[0] for d in soup.find_all("span",{"class":"price-range"})]
+area = [d.contents[0].strip() for d in soup.find_all("span", {"class":"neighborhood-str-list"})]
+addr = [d.contents[0].strip() for d in soup.find_all("address")]
+
+
+res = [d for d in zip(bn, rating, price, area, addr)]
+with open('results.csv', 'w') as csvfile:
+    w = csv.writer(csvfile)
+    w.writerow(["Restaurant", "Rating", "Price_Range", "Area", "Address"])
+    try:
+        for e in res:
+            w.writerow(e)
+    except:
+        pass
