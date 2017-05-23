bf811de8a4a0b48bda65d7d775c50a49ad1d470d
dzou <dawenzou1@gmail.com>
2017-05-22 21:32:24 -0400

    added pie chart to analyzer

--- YelpNYC/YelpAnalyze.py
+++ YelpNYC/YelpAnalyze.py
@@ -16,14 +16,31 @@ class analyzer(object):
 
     def CountPriceRange(self):
         # columns: name(varchar(255)), rating(decimal), price(varchar(6)), area(varchar(255)), addr(varchar(255))
-        self.priceRangeCount = pd.DataFrame(self.resData.groupby('price').size().rename(' count '))
+        self.priceRangeCount = pd.DataFrame(self.resData.groupby('price').size().rename('count')).sort_values(by=['count'], ascending=False)
+        print self.priceRangeCount
+        #self.ranges = self.priceRangeCount.set_index("price")
         #store the priceRangeCount in a pandas data frame format; else it'll print with all the column names as well
-        WantToPrint = raw_input("would you like to see the results?(Y/N) ")
+        WantToPrint = raw_input("would you like to see the Pie Chart?(Y/N) ")
         if WantToPrint == 'Y' or WantToPrint == 'y':
             #plt.figure(figsize=(8,8))
             #self.resData.groupby('price').count().plot(kind='pie', y = 'count', startangle = 90, shadow = False, labels = self.resData['price'], legend = False, fontsize=14)
             #plt.show()
-            print self.priceRangeCount
+            labels = self.priceRangeCount.index.values.tolist()
+            for i,e in enumerate(labels):
+                if e == '$':
+                    labels[i] = "\$"
+                if e == '$$':
+                    labels[i] = "\$\$"
+                if e == '$$$':
+                    labels[i] = "\$\$\$"
+                if e == '$$$$':
+                    labels[i] = "\$\$\$\$"
+            size = self.priceRangeCount['count'].values.tolist()
+            fig1, ax1 = plt.subplots(figsize=(16, 8))
+            ax1.pie(size, labels=labels, autopct='%1.1f%%',shadow=True, startangle=90)
+            ax1.axis('equal')
+            plt.savefig('piechart.png')
+            plt.show()
         else:
             pass
 
